26ea627b60ac7345d23a23c6b9bb29ae0816c0ec
Kang TaeWook <tw2.kang@kt.com>
2025-03-31 18:01:36 +0900

    커밋 3번째 test_calc.py 수정 pass로 처리되도록 값 변경 NEXTSCHEME-51

--- test_calc.py
+++ test_calc.py
@@ -7,7 +7,7 @@ class TestMathFunctions(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
     
     def test_subtract(self):
-        self.assertEqual(subtract(5, 3), 1)
+        self.assertEqual(subtract(5, 3), 2)
         self.assertEqual(subtract(0, 3), -3)
 
 if __name__ == '__main__':
