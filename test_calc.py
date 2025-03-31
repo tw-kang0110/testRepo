f5fe8cf51389ba4b602021144f51d1c16476ce75
Kang TaeWook <tw2.kang@kt.com>
2025-03-31 17:42:02 +0900

    first commit

---
+++ test_calc.py
@@ -0,0 +1,14 @@
+import unittest
+from calc import add, subtract
+
+class TestMathFunctions(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+    
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(0, 3), -3)
+
+if __name__ == '__main__':
+    unittest.main()
